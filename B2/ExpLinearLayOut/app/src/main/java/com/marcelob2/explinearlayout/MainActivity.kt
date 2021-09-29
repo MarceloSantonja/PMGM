40ea18ced9fb3fb3331ecc03e72226f6a2151541
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-09-29 12:05:34 +0200

    añadiendo layout

---
+++ B2/ExpLinearLayOut/app/src/main/java/com/marcelob2/explinearlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.marcelob2.explinearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.cardview)
+    }
+}
