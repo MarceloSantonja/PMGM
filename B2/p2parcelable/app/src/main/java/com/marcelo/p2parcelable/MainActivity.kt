522853e795e6b35e9988db47d7f1d5c0706e221b
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-10-06 11:21:15 +0200

    tool

--- B2/p2parcelable/app/src/main/java/com/marcelo/p2parcelable/MainActivity.kt
+++ B2/p2parcelable/app/src/main/java/com/marcelo/p2parcelable/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.TextView
 import com.marcelob2.personalibrary.Persona
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
