f54edf386d438759c93d0ed4102101fbee0fdff7
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-10-08 11:15:03 +0200

    botones ICO Outline Unelevate

--- B2/EjemploBoton/app/src/main/java/com/marcelo/ejemploboton/MainActivity.kt
+++ B2/EjemploBoton/app/src/main/java/com/marcelo/ejemploboton/MainActivity.kt
@@ -2,15 +2,40 @@ package com.marcelo.ejemploboton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.marcelo.ejemploboton.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var bindig:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindig = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindig.root)
-        bindig.texto.text = "Soy el texto cambiado con el Binding"
+        bindig.boton.setOnClickListener{
+            Toast.makeText(applicationContext,"Boton",Toast.LENGTH_SHORT).show()
+        }
+        bindig.toogglegroup.addOnButtonCheckedListener { group, checkedId, isChecked ->  }
+        bindig.boton0.setOnClickListener(this)
+        bindig.materialButtonIcon.setOnClickListener(this)
+        bindig.buttonUnelevate.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0){
+
+            bindig.boton0 -> Toast.makeText(applicationContext,"Boton0",Toast.LENGTH_SHORT).show()
+            bindig.materialButtonIcon -> Toast.makeText(applicationContext,"BotonIco",Toast
+                .LENGTH_SHORT).show()
+            bindig.buttonUnelevate -> Toast.makeText(applicationContext,"BotonUnelevate",Toast
+                .LENGTH_SHORT).show()
+        }
+    }
+    fun miPulsacion(v:View){
+
+        Toast.makeText(applicationContext,"ajsfhjkafghalkfslkjqas",Toast
+            .LENGTH_SHORT).show()
+
     }
 }
