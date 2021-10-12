553feb02728e972fad524f4721f0947e80d17717
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-10-12 22:11:55 +0200

    ejercicio carrito

--- B2/CarritoCompra/app/src/main/java/com/marcelo/carritocompra/MainActivity.kt
+++ B2/CarritoCompra/app/src/main/java/com/marcelo/carritocompra/MainActivity.kt
@@ -2,10 +2,24 @@ package com.marcelo.carritocompra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marcelo.carritocompra.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.AddButton.setOnClickListener {
+
+            var texto:String = binding.ContadorTexto.text.toString()
+            binding.ContadorTexto.setText((texto.toInt() + 1 ).toString())
+
+        }
+
     }
 }
