0d642482fecf23c54bc65d6a7e5a3089bfd648bf
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-10-17 22:09:26 +0200

    radiobutton

--- B5/RadioButton/app/src/main/java/com/marcelo/radiobutton/MainActivity.kt
+++ B5/RadioButton/app/src/main/java/com/marcelo/radiobutton/MainActivity.kt
@@ -2,10 +2,35 @@ package com.marcelo.radiobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import com.marcelo.radiobutton.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val corporativo =binding.CorporativoLayout
+        val particular = binding.ParticularLayout
+
+        binding.ParticularRadioButton.id
+
+        binding.GrupoRadioBotones.setOnCheckedChangeListener { _, _ ->
+
+            if (binding.CorporativoRadioButton.isChecked) {
+                corporativo.setVisibility(View.VISIBLE)
+                particular.setVisibility(View.GONE)
+            }
+            if (binding.ParticularRadioButton.isChecked) {
+                corporativo.setVisibility(View.GONE)
+                particular.setVisibility(View.VISIBLE)
+
+            }
+        }
     }
 }
