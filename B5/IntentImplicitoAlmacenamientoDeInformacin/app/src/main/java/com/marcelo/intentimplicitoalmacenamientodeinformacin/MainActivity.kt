d548b77c4f657477d60caf10c8d88a6168f8cc65
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-10-26 19:02:30 +0200

    aaa

--- B5/IntentImplicitoAlmacenamientoDeInformacin/app/src/main/java/com/marcelo/intentimplicitoalmacenamientodeinformacin/MainActivity.kt
+++ B5/IntentImplicitoAlmacenamientoDeInformacin/app/src/main/java/com/marcelo/intentimplicitoalmacenamientodeinformacin/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.marcelo.intentimplicitoalmacenamientodeinformacin
 
+import android.app.SearchManager
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.material.snackbar.Snackbar
 import com.marcelo.intentimplicitoalmacenamientodeinformacin.databinding.ActivityMainBinding
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +23,35 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val telefonoButton = binding.telefonoImageButton
+        val imagenButton = binding.imageView
+        val emailButton = binding.mailImageButton
+        val REQUEST_PHONE_CALL = 1
+
+        telefonoButton.setOnClickListener (View.OnClickListener {
+            if(binding.telefonoEditText.text.toString()!=null){
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission))
+
+
+                empezarLLamada(binding.telefonoEditText.text.toString())
+
+            }else{
+
+                Toast.makeText(this,"falla mucho",Toast.LENGTH_SHORT).show()
+
+            }
+        })
+
+
     }
+    fun empezarLLamada(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
 }
