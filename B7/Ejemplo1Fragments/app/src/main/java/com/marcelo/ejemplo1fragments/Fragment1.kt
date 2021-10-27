9fc46a91565c382e9b44ec51ebb8573caf03d3e5
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2021-10-27 12:03:40 +0200

    aa

--- B7/Ejemplo1Fragments/app/src/main/java/com/marcelo/ejemplo1fragments/Fragment1.kt
+++ B7/Ejemplo1Fragments/app/src/main/java/com/marcelo/ejemplo1fragments/Fragment1.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 
 class Fragment1 : Fragment() {
+    private val model:transferirDatos by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -14,6 +18,12 @@ class Fragment1 : Fragment() {
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         var vista = inflater.inflate(R.layout.fragment1,container,false)
+        vista.findViewById<Button>(R.id.boton).setOnClickListener {
+        model.setItem(vista.findViewById<EditText>(R.id.editText).text.toString())
+
+        }
+
+
         return vista
     }
 }
