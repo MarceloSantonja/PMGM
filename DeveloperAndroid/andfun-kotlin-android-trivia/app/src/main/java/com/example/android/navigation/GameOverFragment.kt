38833f75d765c0a4a6862258402e11e35b6a0d77
MarceloSantonja <marcelo.santonja@iesdoctorbalmis.com>
2022-04-05 08:45:37 +0200

    aaa

--- DeveloperAndroid/andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ DeveloperAndroid/andfun-kotlin-android-trivia/app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -29,10 +29,12 @@ class GameOverFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
         binding.tryAgainButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragmentToGameFragment())
+            view.findNavController().navigate(GameOverFragmentDirections
+                .actionGameOverFragmentToGameFragment())
         }
         return binding.root
     }
--- EjerciciosPractica/pruebas/app/src/main/java/com/marcelo/pruebas/MainActivity.kt
+++ EjerciciosPractica/pruebas/app/src/main/java/com/marcelo/pruebas/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.marcelo.pruebas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val texto = findViewById<TextView>(R.id.texto)
+
     }
 }
